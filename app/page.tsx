a50b8fa34fdf2db97c6acaa77d89539745e71a4d
Kent Niño Paler <kxzaishteru@gmail.com>
2026-07-29 13:37:07 +0800

    feat(about): add professional about section

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,18 @@
 import { Navbar } from "@/components/layout/Navbar";
 import { Container } from "@/components/layout/Container";
 import { Hero } from "@/components/home/Hero";
+import { About } from "@/components/about/About";
 
 export default function Home() {
   return (
     <>
       <Navbar />
 
-      <main className="flex-1">
-        <Container>
-          <Hero />
-        </Container>
-      </main>
+      <Container>
+        <Hero />
+
+        <About />
+      </Container>
     </>
   );
 }
